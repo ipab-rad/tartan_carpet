b07adf0391524d10ff58ed4a3c050e3f0359527e
Hector Cruz <37127013+hect95@users.noreply.github.com>
2024-10-22 12:11:13 +0100

    Enhance run_sensors.sh (#24)
    - Rename `dev_*` compose files to `local_*` to align better to their
    context
    - Add `--local` flag to run `local_sensors_compose.yaml` instead with
    docker compose
    - Add `--build` flag to locally build runtime (`latest`) sensor docker
    images
    - Support sensor docker list to select which sensors to run with
    `run_sensors.sh`
    - Add missing sensors in `local_*` compose files and volumes mounting
    
    ---------
    Authored by: Hector Cruz (@hect95)
    Co-authored-by: Alejandro Bordallo (@GreatAlexander )

--- run_sensors.sh
+++ run_sensors.sh
@@ -1,18 +1,111 @@
 #!/bin/bash
 
+help() {
+    echo "Usage: run_sensors.sh [options] [sensor_list]
+
+    Options:
+      --local        Use the 'local_sensors_compose.yaml' file
+      --build      Build Docker images for the specified sensors
+      --no-cache   Build Docker images with no cache
+      -h, --help   Show this help message and exit
+    "
+    exit 0
+}
+
 # Function to handle termination
 stop_sensors() {
     echo "Caught signal, stopping Docker Compose..."
-    docker compose -f ./sensors_compose.yaml kill
-    docker compose -f ./sensors_compose.yaml down
+    docker compose -f "$compose_file" kill
+    docker compose -f "$compose_file" down
     exit 0
 }
 
 # Trap SIGINT and SIGTERM
 trap 'stop_sensors' SIGINT SIGTERM
 
-# Start Docker Compose
-docker compose -f ./sensors_compose.yaml up &
+# Default values
+compose_file="./sensors_compose.yaml"
+services=""
+build_docker=""
+run_action_count=0  # Counter to track --local and --build
+build_no_cache=""
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --local)
+            compose_file="./local_sensors_compose.yaml"
+            ((run_action_count++))  # Increment counter for --local
+            shift
+            ;;
+        --build)
+            build_docker="True"
+            ((run_action_count++))  # Increment counter for --build
+            shift
+            ;;
+        --no-cache)
+            build_no_cache="--no-cache"
+            shift
+            ;;
+        -h|--help)
+            help
+            ;;
+        *)
+            services="$services $1"
+            shift
+            ;;
+    esac
+done
+
+build_docker_images() {
+    original_dir=$(pwd)  # Save the original directory
+    for service in $services; do
+        found=0
+        # Search recursively for directories matching the service name
+        for dir in $(find . -type d -name "$service"); do
+            # Check for Dockerfile in the directory
+            if [[ -f "$dir/Dockerfile" ]]; then
+                echo "Building Docker image for $service..."
+                pushd "$dir" > /dev/null   # Change to the directory containing the Dockerfile
+                docker build $build_no_cache -t "$service:latest" --target runtime .
+                popd > /dev/null   # Return to the original directory
+                found=1
+                break
+            fi
+        done
+        # If no matching Dockerfile or directory is found
+        if [[ $found -eq 0 ]]; then
+            echo "No directory or Dockerfile found for $service."
+        fi
+    done
+}
+
+# If both --local and --build are provided, first build, then run Docker Compose
+if [ "$run_action_count" -eq 2 ]; then
+    if [ -n "$services" ]; then
+        build_docker_images
+        docker compose -f "$compose_file" up $services &
+    else
+        echo "No services specified for building."
+        exit 1
+    fi
+
+    # If only --build is provided, build the Docker images but don't run services
+elif [ -n "$build_docker" ]; then
+    if [ -n "$services" ]; then
+        build_docker_images
+    else
+        echo "No services specified for building."
+        exit 1
+    fi
+
+    # Default case: run Docker Compose with the defined compose file
+else
+    if [ -n "$services" ]; then
+        docker compose -f "$compose_file" up $services &
+    else
+        docker compose -f "$compose_file" up &
+    fi
+fi
 
 # Wait for Docker Compose to finish
 wait $!
